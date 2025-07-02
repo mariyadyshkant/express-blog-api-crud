f920c006b211e4b5d5ce88b42a8054263bafe81b
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-07-02 18:12:07 +0200

    feat: integrate posts router into the server setup

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const postsRouter = require('./routers/posts');
 app.use(express.static('public'));
+app.use('/posts', postsRouter);
+
 
 app.get('/', (req, res) => {
     res.send('Questo è il server del mio blog');
