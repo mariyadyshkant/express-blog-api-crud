6e26011e7f34c74a87c64d1efcfa4db06af5b1eb
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-07-02 18:09:23 +0200

    refactor: clean up post router code and improve delete response handling

--- routers/posts.js
+++ routers/posts.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const PORT = process.env.PORT;
 const posts = require('../data/posts.js');
-const { log } = require('node:console');
+const { appendFile } = require('fs');
+// const { log } = require('node:console');
+
+router.use(express.json());
 
 // Index
 router.get('/', (req, res) => {
@@ -24,22 +27,39 @@ router.get('/:id', (req, res) => {
     }
 });
 
+//Store
+router.post('/', (req, res) => {
+    const newId = posts[posts.length - 1].id + 1;
+
+    const newPost = {
+        id: newId,
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+        tags: req.body.tags
+    };
+
+    posts.push(newPost);
+    console.log(posts);
+    res.status(201);
+    res.json(newPost);
+});
+
 // Destroy
 router.delete('/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const post = posts.find(post => post.id === id);
 
-    if (post) {
-        res.status(204);
-        posts.splice(posts.indexOf(post), 1);
-        res.json({
-            message: `Post with id ${id} deleted successfully`
-        });
-        console.log(posts);
-    } else {
+    if (!post) {
         res.status(404);
         return res.json({
             message: `Post with id ${id} not found`
         });
     }
+
+    posts.splice(posts.indexOf(post), 1);
+    console.log(posts);
+    res.status(204);
 });
+
+module.exports = router;
