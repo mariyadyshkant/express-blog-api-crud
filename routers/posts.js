3fef0bd6d32478f4b533b8b650e360806ad64b61
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-07-03 11:53:38 +0200

    feat: implement update functionality for posts in the router

--- routers/posts.js
+++ routers/posts.js
@@ -45,6 +45,31 @@ router.post('/', (req, res) => {
     res.json(newPost);
 });
 
+// Update
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(post => post.id === id);
+
+    if (!post) {
+        res.status(404);
+        return res.json({
+            message: `Post with id ${id} not found`
+        });
+    }
+
+    const updatedPost = {
+        id: post.id,
+        title: req.body.title || post.title,
+        content: req.body.content || post.content,
+        image: req.body.image || post.image,
+        tags: req.body.tags || post.tags
+    };
+
+    posts[posts.indexOf(post)] = updatedPost;
+    console.log(posts);
+    res.json(updatedPost);
+});
+
 // Destroy
 router.delete('/:id', (req, res) => {
     const id = parseInt(req.params.id);
